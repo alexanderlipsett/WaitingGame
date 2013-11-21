38dbfa09a8385f03433d0c69dbb2fcdaacd34786
alexanderlipsett <alexander.lipsett@gmail.com>
2013-11-20 20:06:49 -0800

    added incorrect response view

---
+++ WaitingGame/IncorrectViewController.h
@@ -0,0 +1,13 @@
+//
+//  IncorrectViewController.h
+//  WaitingGame
+//
+//  Created by Alex Lipsett on 11/20/13.
+//  Copyright (c) 2013 Alex Lipsett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IncorrectViewController : UIViewController
+
+@end
