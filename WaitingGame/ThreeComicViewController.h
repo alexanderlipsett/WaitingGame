9728196fc33787101db328e75d3c4f9c9c4e2f0c
alexanderlipsett <alexander.lipsett@gmail.com>
2013-11-20 17:59:43 -0800

    Added test plist, 3panel view

---
+++ WaitingGame/ThreeComicViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThreeComicViewController.h
+//  WaitingGame
+//
+//  Created by Alex Lipsett on 11/20/13.
+//  Copyright (c) 2013 Alex Lipsett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeComicViewController : UIViewController
+
+@end
