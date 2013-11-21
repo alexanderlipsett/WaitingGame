905a995b0d5c18a9be8d7b60a848c3fb6fec0a8d
alexanderlipsett <alexander.lipsett@gmail.com>
2013-11-20 21:47:40 -0800

    various changes

---
+++ WaitingGame/SummaryViewController.m
@@ -0,0 +1,38 @@
+//
+//  SummaryViewController.m
+//  WaitingGame
+//
+//  Created by Alex Lipsett on 11/20/13.
+//  Copyright (c) 2013 Alex Lipsett. All rights reserved.
+//
+
+#import "SummaryViewController.h"
+
+@interface SummaryViewController ()
+
+@end
+
+@implementation SummaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
