62d4210835da21df162e70456d6acbfc13edd78c
alexanderlipsett <alexander.lipsett@gmail.com>
2013-11-21 23:29:00 -0800

    finished plist reading

--- WaitingGame/Quiz.m
+++ WaitingGame/Quiz.m
@@ -154,8 +154,11 @@
     NSString *badSummaryText = [summary objectForKey:@"badSummaryText"];
     NSNumber *threshold = [summary objectForKey:@"goodThreshold"];
     
-    sum.goodTitle
-
+    sum.goodComic = goodSummaryImage;
+    sum.badComic = badSummaryImage;
+    sum.goodDescription = goodSummaryText;
+    sum.badDescription = badSummaryText;
+    sum.threshold = threshold;
     
 }
 
