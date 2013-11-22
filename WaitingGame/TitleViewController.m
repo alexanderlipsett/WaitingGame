2e280c1c77931759276fcce9253696f444f38801
alexanderlipsett <alexander.lipsett@gmail.com>
2013-11-21 22:39:03 -0800

    Revert back to old change`

---
+++ WaitingGame/TitleViewController.m
@@ -0,0 +1,49 @@
+//
+//  TitleViewController.m
+//  WaitingGame
+//
+//  Created by Derrick J Chie on 11/21/13.
+//  Copyright (c) 2013 Alex Lipsett. All rights reserved.
+//
+
+#import "TitleViewController.h"
+#import "MenuViewController.h"
+
+@interface TitleViewController ()
+
+@end
+
+@implementation TitleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    UIColor *background = [[UIColor alloc] initWithRed:244.0f/255.0f green:241.0f/255.0f blue:215.0f/255.0f alpha:1.0f];
+    self.view.backgroundColor = background;
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)beginButtonTapped:(id)sender
+{
+    MenuViewController *menuViewController = [[MenuViewController alloc] init];
+    [self.navigationController pushViewController:menuViewController animated:YES];
+}
+
+@end
